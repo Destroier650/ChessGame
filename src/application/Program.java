382f15adb7c0a4bc784a018b1514e946fdf6d71d
Alef Emanuel <alefemanuelmv@gmail.com>
2024-11-19 19:16:54 -0300

    Testing bug on commit

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,10 @@
-public class Main {
-    public static void main(String[] args) {
+package application;
 
+import chess.ChessMatch;
+
+public class Program {
+    public static void main(String[] args) {
+        ChessMatch chessMatch = new ChessMatch();
+        UI.printBoard(chessMatch.getPieces());
     }
 }
