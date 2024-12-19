cb9ebaed2029932bbbafa56bc10e523d4067c7ec
Destroier650 <alefemanuelmv@gmail.com>
2024-12-19 17:39:01 -0300

    added all the pieces

--- src/chess/ChessPosition.java
+++ src/chess/ChessPosition.java
@@ -7,10 +7,12 @@ public class ChessPosition {
     private char column;
     private int row;
 
-    public ChessPosition(char column, int row){
-        if (column < 'a' || column > 'h' || row < 1 || row > 8){
-            throw new ChessException("Error instantiating ChessPosition. Valid Values are a1 to h8");
+    public ChessPosition(char column, int row) {
+        if (column < 'a' || column > 'h' || row < 1 || row > 8) {
+            throw new ChessException("Error instantiating ChessPosition. Valid values are from a1 to h8.");
         }
+        this.column = column;
+        this.row = row;
     }
 
     public char getColumn() {
@@ -21,17 +23,16 @@ public class ChessPosition {
         return row;
     }
 
-    protected Position toPosition () {
+    protected Position toPosition() {
         return new Position(8 - row, column - 'a');
     }
 
     protected static ChessPosition fromPosition(Position position) {
-        return new ChessPosition((char)('a' - position.getColumn()), 8 - position.getRow());
+        return new ChessPosition((char)('a' + position.getColumn()), 8 - position.getRow());
     }
 
     @Override
-    public String toString () {
+    public String toString() {
         return "" + column + row;
     }
-
 }
